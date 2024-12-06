1e3fe2b5c27a7501e01fa47d1f71957c54b3bb7c
carendsen <collinarendsen@gmail.com>
2024-12-06 19:14:09 +0800

    test

--- pages/Report.py
+++ pages/Report.py
@@ -11,6 +11,23 @@ st.title("Analysis of Results")
 st.markdown("---")
 
 
+# api to display current date
+
+url = "http://worldtimeapi.org/api/timezone/Etc/UTC"
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an HTTPError for bad responses
+    data = response.json()
+    today_date = data.get("datetime", "").split("T")[0]
+
+except requests.RequestException as e:
+    print(f"An error occurred while fetching the date: {e}")
+except ValueError:
+    print("Error decoding JSON response. Please check the API response format.")
+
+
+
 # Initialize session state for responses
 if 'responses' not in st.session_state:
     st.session_state.responses = None  # Default to None if not filled out yet
@@ -21,8 +38,10 @@ if st.session_state.responses:
         # Retrieve responses from session state
         age, gender_numeric, parental_degree_numeric, average_time, absences, tutoring_numeric, support_numeric, extracurricular, sports, music, volunteering, performance = st.session_state.responses
 
-        st.write("hello")
-
+        st.subheader(f"Report of {today_date}")
+        st.write("Below is a comparison of your inputs against the overall average (see Figure 1) and your predicted grade based on your inputs (see Figure 2).")
+        st.write("")
+        st.write("")
     except ValueError:
         # Handle error if unpacking fails (e.g., the list doesn't have 12 elements)
         st.error("Error: Incorrect number of responses or malformed data.")
